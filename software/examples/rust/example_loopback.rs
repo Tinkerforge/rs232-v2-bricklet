7e60bab433a5d922e9e0f823afc847ea699ec922
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:27:15 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_loopback.rs
+++ software/examples/rust/example_loopback.rs
@@ -1,5 +1,5 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, rs232_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, rs232_v2_bricklet::*};
 
 // For this example connect the RX pin to the TX pin on the same Bricklet
 
@@ -9,18 +9,18 @@ const UID: &str = "XYZ"; // Change XYZ to the UID of your RS232 Bricklet 2.0
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let rs232_v2_bricklet = RS232V2Bricklet::new(UID, &ipcon); // Create device object
+    let rs232 = Rs232V2Bricklet::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
-    //Create listener for read events.
-    let read_listener = rs232_v2_bricklet.get_read_receiver();
-    // Spawn thread to handle received events. This thread ends when the rs232_v2_bricklet
+    //Create receiver for read events.
+    let read_receiver = rs232.get_read_receiver();
+    // Spawn thread to handle received events. This thread ends when the rs232
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in read_listener {
-            match event {
+        for read in read_receiver {
+            match read {
                 Some((payload, _)) => {
                     let message: String = payload.iter().collect();
                     println!("Message (Length: {}) {}", message.len(), message);
@@ -31,10 +31,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Enable read callback
-    rs232_v2_bricklet.enable_read_callback();
+    rs232.enable_read_callback();
 
     // Write "test" string
-    rs232_v2_bricklet.write(&['t', 'e', 's', 't'])?;
+    rs232.write(&['t', 'e', 's', 't'])?;
 
     println!("Press enter to exit.");
     let mut _input = String::new();
