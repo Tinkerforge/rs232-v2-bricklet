22c3db118fb55bfd5353e0d1bc20098f8e2a8f94
Matthias Bolte <matthias@tinkerforge.com>
2022-04-26 13:56:32 +0200

    uc: Adapt to current bindings

--- software/examples/uc/example_loopback.c
+++ software/examples/uc/example_loopback.c
@@ -1,59 +1,41 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
 #include <string.h>
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_rs232_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_rs232_v2.h"
 
 // For this example connect the RX1 and TX pin to receive the send message
 
-#define UID "XYZ" // Change XYZ to the UID of your RS232 Bricklet 2.0
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
 
-void check(int rc, const char* msg);
-
-static char buffer[61] = {0}; // + 1 for the null terminator.
+static char buffer[5] = {0}; // +1 for the null terminator
 
 // Callback function for read callback
-static void read_low_level_handler(TF_RS232V2 *device, uint16_t message_length, uint16_t message_chunk_offset, char message_chunk_data[60], void *user_data) {
+static void read_handler(TF_RS232V2 *device, char *message, uint16_t message_length,
+                         void *user_data) {
 	(void)device; (void)user_data; // avoid unused parameter warning
 
-	bool last_chunk = message_chunk_offset + 60 > message_length;
+	message[message_length] = '\0';
 
-	uint16_t to_copy = 60;
-	if (last_chunk) {
-		//This is the last chunk, only read the valid part of the message
-		to_copy = message_length - message_chunk_offset;
-	}
-
-	memcpy(buffer, message_chunk_data, to_copy);
-	buffer[to_copy] = '\0';
-
-	if(message_chunk_offset == 0) {
-		tf_hal_printf("Message: \"");
-	}
-
-	tf_hal_printf("%s", buffer);
-	if(last_chunk) {
-		tf_hal_printf("\"\n");
-	}
+	tf_hal_printf("Message: \"%s\"\n", message);
 }
 
 static TF_RS232V2 rs232;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_rs232_v2_create(&rs232, UID, hal), "create device object");
+	check(tf_rs232_v2_create(&rs232, NULL, hal), "create device object");
 
-
-	// Register read callback to function read_low_level_handler
-	tf_rs232_v2_register_read_low_level_callback(&rs232,
-	                                             read_low_level_handler,
-	                                             NULL);
+	// Register read callback to function read_handler
+	tf_rs232_v2_register_read_callback(&rs232,
+	                                   read_handler,
+	                                   buffer,
+	                                   NULL);
 
 	// Enable read callback
 	check(tf_rs232_v2_enable_read_callback(&rs232), "call enable_read_callback");
